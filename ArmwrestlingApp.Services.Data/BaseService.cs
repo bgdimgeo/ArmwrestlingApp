81fd70fd6d491f5c52c3b815d135340e0a31793f
Nan <Acer@top.fg>
2025-06-14 11:35:19 +0300

    Base Controller and extension method for service provider

---
+++ ArmwrestlingApp.Services.Data/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using ArmwrestlingApp.Services.Data.Interfaces;
+
+namespace ArmwrestlingApp.Services.Data
+{
+    public class BaseService: IBaseService
+    {
+        public bool IsGuidValid(string? id, ref Guid parsedGuid)
+        {
+            // Non-existing parameter in the URL
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            // Invalid parameter in the URL
+            bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ArmwrestlingApp/Controllers/BaseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArmwrestlingApp.Controllers
+{
+    public class BaseController : Controller
+    {
+
+        protected bool IsGuidValid(string? id, ref Guid parsedGuid)
+        {
+            // Non-existing parameter in the URL
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            // Invalid parameter in the URL
+            bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected string? GetCurrentUserId() // nullable che moje dane vurne stoinost
+        {
+            //return User.FindFirstValue("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+            // moje i ot usermanagera ama se rovi v bazata po-dobre ot cookieto 
+
+        }
+
+        protected bool IsUserAuthenticated()
+        {
+
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
